3db9ae39b7d10be3d07d82c71d563b1273182325
kale gauri <kalegauri2024@gmail.com>
2026-04-09 22:13:10 +0530

    images  added

--- src/data/vegetables.js
+++ src/data/vegetables.js
@@ -3,19 +3,19 @@ const vegetables = [
     id: 1,
     name: "Tomato",
     price: 20,
-    image: "https://via.placeholder.com/150"
+    image: "src/assets/tomato.png"
   },
   {
     id: 2,
     name: "Potato",
     price: 25,
-    image: "https://via.placeholder.com/150"
+    image: "src/assets/potato.png"
   },
   {
     id: 3,
     name: "Onion",
     price: 30,
-    image: "https://via.placeholder.com/150"
+    image: "src/assets/onion.png"
   }
 ];
 
